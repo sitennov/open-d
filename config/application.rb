05145d8a862f983cda9b122043a5bedfde7e12a3
theprosto <sitennov@mail.ru>
2017-06-25 23:32:35 +0300

    fix: home to adverts, config application

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,18 @@ module OpenD
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.generators do |g|
+      g.test_framework :rspec,
+                       fixtures: true,
+                       view_spec: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       request_specs: false,
+                       controller_spec: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+    end
+
+    # config.i18n.default_locale = :ru
   end
 end
